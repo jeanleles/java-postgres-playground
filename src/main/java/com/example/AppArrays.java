1842eab22a59bcb9c143cb8d107b592b345131a6
Jean Leles <jeanleles.sa@gmail.com>
2023-08-05 22:53:18 +0000

    Arrays finished!

--- src/main/java/com/example/AppArrays.java
+++ src/main/java/com/example/AppArrays.java
@@ -26,7 +26,7 @@ public class AppArrays {
     matrix[0] = new double[]{1, 2, 3, 4, 5};
     System.out.println(Arrays.toString(matrix[0]));
     
-    double matrix2[][] = new double[2][3];
+    double matrix2[][] = new double[5][3];
     for (int i = 0; i < matrix2.length; i++) {
       for (int j = 0; j < matrix2[i].length; j++) {
         matrix2[i][j] = (i * matrix2[i].length + j+1) * 10; 
@@ -35,7 +35,7 @@ public class AppArrays {
     System.out.println("********************");
     for (int i = 0; i < matrix2.length; i++) {
       for (int j = 0; j < matrix2[i].length; j++) {
-        System.out.print(matrix2[i][j] + ", "); 
+        System.out.printf("%5.1f ", matrix2[i][j]); 
       }
       System.err.println();
     }
