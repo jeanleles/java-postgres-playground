0108531976583a99d2b66297a15d84fadf9f8cb9
Jean Leles <jeanleles.sa@gmail.com>
2023-08-19 16:59:41 +0000

    opeadrores comparação

--- src/main/java/com/example/AppOperatorsRelacionais.java
+++ src/main/java/com/example/AppOperatorsRelacionais.java
@@ -3,15 +3,26 @@ package com.example;
 public class AppOperatorsRelacionais {
   public static void main(String[] args) {
     var cliente1 = new Cliente();
-    cliente1.setAnoNascimento(1990); 
+    cliente1.setAnoNascimento(1999); 
     var cliente2 = new Cliente();
-    cliente2.setAnoNascimento(1990);
+    cliente2.setAnoNascimento(1991);
 
     if (cliente1.getAnoNascimento() == cliente2.getAnoNascimento()) {
       System.out.println("Clientes 1 e 2 nasceram no mesmo ano!");
     }
     else {
       System.out.println("Clientes 1 e 2 nasceram em anos diferntes.");
-    } 
+    }
+    
+    boolean cliente1MaisJovem = cliente1.getAnoNascimento() > cliente2.getAnoNascimento();
+    boolean cliente1MaisVelho = cliente1.getAnoNascimento() < cliente2.getAnoNascimento();
+    System.out.println("------------------------");
+    if(cliente1MaisJovem) {
+      System.out.println("Cliente1 eh mais jovem");
+    }
+    else if (cliente1MaisVelho) {
+      System.out.println("Cliente1 eh mais velho");
+    }
+    else System.out.println("Clientes 1 e 2 tem a mesma idade!");
   }
 }
